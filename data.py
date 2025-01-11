1f57b148d93d6d17dd53a33f7f5c9f0d64b2507a
Aleksandr Zhukov <hyperpugs@yandex.ru>
2025-01-11 21:08:01 +0300

    first

---
+++ data.py
@@ -0,0 +1,18 @@
+class Urls:
+    MAIN_PAGE = 'https://stellarburgers.nomoreparties.site/'
+    RECOVERY_PAGE = f"{MAIN_PAGE}forgot-password"
+    RESET_PAGE = f"{MAIN_PAGE}reset-password"
+    ORDER_HISTORY_PAGE = f"{MAIN_PAGE}account/order-history"
+    ORDER_FEED_PAGE = f"{MAIN_PAGE}feed"
+
+class Buttons:
+    PROFILE_BUTTON_TEXT = 'Профиль'
+    ENTER_BUTTON_TEXT = 'Войти'
+    ORDER_DETAILS = 'Детали ингредиента'
+    ORDER_IS_CREATING = 'Ваш заказ начали готовить'
+    COMPOUND_TEXT = 'Cостав'
+
+class TestData:
+
+    test_email = 'samarskiy@ya.com'
+    test_password = '123456qwe'
