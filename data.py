defdc4c7877ae16935321e8f88ca03578374692c
Aleksandr Zhukov <hyperpugs@yandex.ru>
2025-01-12 16:34:07 +0300

    pages + tests

--- data.py
+++ data.py
@@ -1,10 +1,14 @@
 class Urls:
     MAIN_PAGE = 'https://stellarburgers.nomoreparties.site/'
     RECOVERY_PAGE = f"{MAIN_PAGE}forgot-password"
+    PROFILE_PAGE = f"{MAIN_PAGE}account/profile"
     RESET_PAGE = f"{MAIN_PAGE}reset-password"
     ORDER_HISTORY_PAGE = f"{MAIN_PAGE}account/order-history"
     ORDER_FEED_PAGE = f"{MAIN_PAGE}feed"
+    LOGIN_PAGE = f"{MAIN_PAGE}login"
 
+    CREATE_USER = f"{MAIN_PAGE}api/auth/register"
+    AUTH_USER = f"{MAIN_PAGE}api/auth/user"
 class Buttons:
     PROFILE_BUTTON_TEXT = 'Профиль'
     ENTER_BUTTON_TEXT = 'Войти'
